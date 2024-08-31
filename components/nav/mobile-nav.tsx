aa19050ee9c395e0d27ac3795bf53e9e3e32f793
djkenneth <dj.kenneth.pineda@gmail.com>
2024-08-31 11:48:55 +0800

    update mobile nav

--- components/nav/mobile-nav.tsx
+++ components/nav/mobile-nav.tsx
@@ -6,7 +6,7 @@ import Link, { LinkProps } from "next/link";
 import { useRouter } from "next/navigation";
 
 // Utility Imports
-import { Menu, ArrowRightSquare } from "lucide-react";
+import { Menu, ArrowRightSquare, X } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 // Component Imports
@@ -31,15 +31,19 @@ export function MobileNav() {
           <span className="sr-only">Toggle Menu</span>
         </Button>
       </SheetTrigger>
-      <SheetContent side="left" className="pr-0">
-        <MobileLink
-          href="/"
-          className="flex items-center"
-          onOpenChange={setOpen}
-        >
-          <ArrowRightSquare className="mr-2 h-4 w-4" />
-          <span className="text-muted-foreground">My Site</span>
-        </MobileLink>
+      <SheetContent side="left" className="px-0 w-full" isClose={true}>
+        <div className="flex justify-between items-center py-3 px-4 bg-primaryBlue">
+          <MobileLink
+            href="/"
+            className="flex items-center"
+            onOpenChange={setOpen}
+          >
+            <span className="text-white font-bold">TECHNO</span>
+          </MobileLink>
+          <Button size="icon" className="bg-transparent" onClick={() => setOpen(false)}>
+            <X size={28} strokeWidth={2.75} className="text-white" />
+          </Button>
+        </div>
         <ScrollArea className="my-4 h-[calc(100vh-8rem)] pb-10 pl-6">
           <div className="flex flex-col space-y-3">
             <h3 className="text-small mt-6">Menu</h3>
@@ -49,13 +53,13 @@ export function MobileNav() {
                 {key.charAt(0).toUpperCase() + key.slice(1)}
               </MobileLink>
             ))}
-            <h3 className="text-small pt-6">Blog Menu</h3>
+            {/* <h3 className="text-small pt-6">Blog Menu</h3>
             <Separator />
             {Object.entries(contentMenu).map(([key, href]) => (
               <MobileLink key={key} href={href} onOpenChange={setOpen}>
                 {key.charAt(0).toUpperCase() + key.slice(1)}
               </MobileLink>
-            ))}
+            ))} */}
           </div>
         </ScrollArea>
       </SheetContent>
